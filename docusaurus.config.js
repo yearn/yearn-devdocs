260b412bc139c167600f9c485d0f10c4092fad30
steffel <2143646+steffenix@users.noreply.github.com>
2021-04-26 11:26:42 +0200

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,86 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Yearn.finance',
+  tagline: 'DeFi made simple',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/yearn-devdocs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'yearn', // Usually your GitHub org/user name.
+  projectName: 'yearn-devdocs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Yearn.finance',
+      logo: {
+        alt: 'YFI Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'getting-started/introduction',
+          label: "Getting started"
+        },
+        {
+          type: 'doc',
+          docId: 'partnership/introduction',
+          label: "Partnership"
+        }, 
+        {
+          type: 'doc',
+          docId: 'developers/introduction',
+          label: "Developers"
+        }, 
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discourse',
+              href: 'https://gov.yearn.finance/',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.yearn.finance/',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/iearnfinance',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/yearn/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} yearn.finance. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/yearn/yearn-devdocs/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title"><img src="img/logo.svg" alt="yearn logo"/></h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/getting-started/introduction">
+            Getting started
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/partnership/introduction">
+            Partnership
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/developers/introduction">
+            Developers
+          </Link>
+
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
