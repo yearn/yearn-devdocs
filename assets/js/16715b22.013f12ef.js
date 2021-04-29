8f6c900296124de16b6d9a40518dbbd0116840d2
gh-actions <actions@github.com>
2021-04-29 14:33:29 +0000

    Deploy website - based on 9843287cd769ee563eca079de0b82698dbdc4673

---
+++ assets/js/16715b22.013f12ef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{75:function(e){e.exports=JSON.parse('{"pluginId":"partners","version":"current","label":"Next","isLast":true,"docsSidebars":{"mySidebar":[{"type":"link","label":"Partnership","href":"/yearn-devdocs/partners/introduction"}]},"permalinkToSidebar":{"/yearn-devdocs/partners/introduction":"mySidebar"}}')}}]);
