d2e000c80719d07ff7024ee0b952d55e8aea1173
gh-actions <actions@github.com>
2021-06-02 09:06:53 +0000

    Deploy website - based on 0a385192c915b5040479325c48d133936b549ba3

--- assets/js/1a53a667.c71f08c9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{83:function(e){e.exports=JSON.parse('{"pluginId":"v_one","version":"current","label":"Next","isLast":true,"docsSidebars":{"mySidebar":[{"type":"link","label":"Developers guide","href":"/yearn-devdocs/v1/introduction"}]},"permalinkToSidebar":{"/yearn-devdocs/v1/introduction":"mySidebar"}}')}}]);
