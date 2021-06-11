f33bba42e9d6d435d2c6e6212d0a4a14137d470e
enie123 <enie123@users.noreply.github.com>
2021-06-11 03:11:43 -0400

    Fix solidity doc formatting (#16)
    * docs: remove @notice tag
    
    * Update package versions (#10)
    
    * chore: make version 0.4.1
    
    * chore: packages versions upgrades
    
    * chore: update to 0.4.2 (#13)
    
    * feat: add handlebar helper for solidity-doc formatting
    
    Co-authored-by: Steffel <2143646+steffenix@users.noreply.github.com>

---
+++ helpers/README.md
@@ -0,0 +1,10 @@
+# Handlebar Helpers
+
+Contains handlebar helper functions that are used for custom formatting of documentation for the solidity contracts 
+
+
+## Using custom helpers with solidity-docgen
+
+There is no documentation, but the original commit is useful: [Commit from solidity docgen repo](https://github.com/OpenZeppelin/solidity-docgen/commit/1b2bf2dc692897c872e3cc8411aa0eae363fbb0e)
+
+
---
+++ helpers/solidityHelpers.js
@@ -0,0 +1,19 @@
+function formatDoc(str) {
+   if (str === undefined) {
+       return undefined; 
+   }
+   return newlineOnTag(removeNoticeTag(str)); 
+}
+
+function removeNoticeTag(str) {
+   return str.replace(/@notice(\s)*/gm, "");
+}
+
+// Inserts an additional new line before tags (e.g. @notice, @dev)
+function newlineOnTag(str) {
+   return str.replace(/\n(\s)*@/gm, "\n\n@"); 
+}
+
+module.exports = {
+    formatDoc
+};
