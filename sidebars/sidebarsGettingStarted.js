fc60f02325bf14ddf29ae24d93e2605f667cb6d7
philburrrt <75766943+philburrrt@users.noreply.github.com>
2021-07-05 18:11:15 -0500

    refactor: add guides to sidebar

--- sidebars/sidebarsGettingStarted.js
+++ sidebars/sidebarsGettingStarted.js
@@ -11,9 +11,14 @@
             items: ['products/yvaults/overview', 'products/yvaults/vaults-and-strategies', 'products/yvaults/vault-tokens', 'products/yvaults/yvault-advantages']
           },
           'products/earn',
-          'products/woofy'
+          'products/woofy',
         ]
       },
+      {
+        type: 'category',
+        label: 'Guides',
+        items: ['guides/how-to-add-a-custom-token-to-metamask', 'guides/how-to-understand-crv-vote-locking', 'guides/how-to-understand-yvault-roi','guides/how-to-understand-yvecrv']
+      }
     ],
   };
   
