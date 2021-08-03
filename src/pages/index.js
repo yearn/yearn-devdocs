9a0d1f05bdf9df03e43db00d213713902597f941
philburrrt <philbertburrow@protonmail.com>
2021-08-02 20:31:14 -0500

    refactor: learn yearn section HTML

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,30 +5,54 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import SearchBar from "@theme-original/SearchBar"
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title"><img src="img/logo.svg" alt="yearn logo"/></h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/v1/introduction">
-            Yearn v1 Docs
+      <div className={styles.container}>
+      <h1 className={styles.title}>Learn Yearn</h1>
+      <center><SearchBar /></center>
+        <div className={styles.row}>
+          <Link style={{ textDecoration: "none", color: "white" }} to="./getting-started/using-yearn">
+            <div className={styles.card}>
+                <h2>Using Yearn</h2>
+            </div>
           </Link>
-          <Link
-          className="button button--secondary button--lg"
-          to="/v2/smart-contracts/vault">
-          Yearn v2 API
+          <Link style={{ textDecoration: "none", color: "white" }} to=".http://localhost:3000/yearn-devdocs/getting-started/products/yvaults/overview">
+            <div className={styles.card}>
+                <h2>Vaults and Strategies</h2>
+            </div>
           </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/partners/introduction">
-            Partners Docs
+          <Link style={{ textDecoration: "none", color: "white" }} to="./getting-started/intro">
+            <div className={styles.card}>
+                <h2>Governance</h2>
+            </div>
           </Link>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.textBox}>
+            <h1 className={styles.header}>Get Connected</h1>
+            <li className={styles.paragraph}>
+            <a href="https://discord.gg/mDyZMVEE">Discord</a>
+            </li>
+            <li className={styles.paragraph}>
+              <a href="https://t.me/yearnfinance">Telegram</a>
+            </li>
+            <li className={styles.paragraph}>
+              <a href="https://gov.yearn.finance/">Governance Forum</a>
+            </li>
+            <li className={styles.paragraph}>
+              <a className={styles.bg} href="./partners/introduction">Partnership Program</a>
+            </li>
+            <li className={styles.paragraph}>
+            <a href="https://twitter.com/iearnfinance">Twitter</a>
+            </li>
+            </div>
+            <div className={styles.learn} />
+          </div>
         </div>
       </div>
     </header>
