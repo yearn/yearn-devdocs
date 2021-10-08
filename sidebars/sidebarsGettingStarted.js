cd3464042b9b1afe7ce5729eafd7c85e1ae3eb02
Kuroge Washu <70884201+KurogeWashu@users.noreply.github.com>
2021-10-08 02:28:19 -0700

    added YIP how-to

--- sidebars/sidebarsGettingStarted.js
+++ sidebars/sidebarsGettingStarted.js
@@ -1,24 +1,23 @@
- module.exports = {
-    mySidebar: [
-      'intro','using-yearn',
-      {
-        type: 'category',
-        label: 'Products',
-        items: [
-          {
-            type: 'category',
-            label: 'yVaults',
-            items: ['products/yvaults/overview', 'products/yvaults/vaults-and-strategies', 'products/yvaults/vault-tokens', 'products/yvaults/yvault-advantages']
-          },
-          'products/earn',
-          'products/woofy',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Guides',
-        items: ['guides/how-to-add-a-custom-token-to-metamask', 'guides/how-to-understand-crv-vote-locking', 'guides/how-to-understand-yvault-roi','guides/how-to-understand-yveCRV']
-      }
-    ],
-  };
-  
+module.exports = {
+  mySidebar: [
+    'intro', 'using-yearn',
+    {
+      type: 'category',
+      label: 'Products',
+      items: [
+        {
+          type: 'category',
+          label: 'yVaults',
+          items: ['products/yvaults/overview', 'products/yvaults/vaults-and-strategies', 'products/yvaults/vault-tokens', 'products/yvaults/yvault-advantages']
+        },
+        'products/earn',
+        'products/woofy',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Guides',
+      items: ['guides/how-to-add-a-custom-token-to-metamask', 'guides/how-to-understand-crv-vote-locking', 'guides/how-to-understand-yvault-roi', 'guides/how-to-understand-yveCRV', 'guides/how-to-make-a-yip.md']
+    }
+  ],
+};
