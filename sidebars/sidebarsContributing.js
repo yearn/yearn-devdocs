3a32e115059607c81e30301ecf209de86fa0c41e
Marco Worms <marcogworms@gmail.com>
2022-06-07 10:40:01 -0300

    add budget process

--- sidebars/sidebarsContributing.js
+++ sidebars/sidebarsContributing.js
@@ -1,22 +1,33 @@
 module.exports = {
-    mySidebar: [
-        'contribute',
-        'contributor-tools',
-        {
-            type:'category',
-            label: 'Operations',
-            items: ['operations/osc-levels','operations/decision-making']
-        },
-        {
-            type: 'category',
-            label: 'Governance',
-            items: ['governance/governance-and-operations', 'governance/proposal-process', 'governance/proposal-repository', 'governance/yfi']
-        },
-        {
-            type: 'category',
-            label: 'Documentation',
-            items: ['documentation/working-on-docs','documentation/writing-style-guide']
-        }
-    ],
-  };
-  
+  mySidebar: [
+    'contribute',
+    'contributor-tools',
+    {
+      type: 'category',
+      label: 'Operations',
+      items: [
+        'operations/osc-levels',
+        'operations/decision-making',
+        'operations/budget',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Governance',
+      items: [
+        'governance/governance-and-operations',
+        'governance/proposal-process',
+        'governance/proposal-repository',
+        'governance/yfi',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Documentation',
+      items: [
+        'documentation/working-on-docs',
+        'documentation/writing-style-guide',
+      ],
+    },
+  ],
+}
