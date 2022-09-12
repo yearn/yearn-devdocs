1cfd37c659db1587d7b0848ab24c5649e7e03c15
Marco Worms <marcogworms@gmail.com>
2022-09-12 15:29:44 -0300

    apply changes on homepage to feature veyfi

--- src/homepageFeatures.js
+++ src/homepageFeatures.js
@@ -3,19 +3,19 @@ export default [
     title: 'Learn',
     rows: [
       {
+        title: 'Getting Started',
+        description: 'Introduction to what is Yearn and why it exists',
+        to: './getting-started/intro',
+      },
+      {
         title: 'Use Yearn',
         description: 'Learn how to use Yearn user interface step by step',
         to: './getting-started/using-yearn',
       },
       {
-        title: 'Vaults & Strategies',
-        description: 'An overview on how Yearn Vaults and its strategies work',
-        to: './getting-started/products/yvaults/overview',
-      },
-      {
-        title: 'Partnership Program',
-        description: `Yearn's partnership program incentivize builders to easily integrate yield into their products and services`,
-        to: './partners/introduction',
+        title: 'Governance',
+        description: 'Learn how people decide the future changes in the Yearn protocol',
+        to: './contributing/governance/governance-and-operations',
       },
     ],
   },
@@ -28,14 +28,34 @@ export default [
         to: './developers/v2/getting-started',
       },
       {
+        title: 'Smart Contracts',
+        description: 'References to our smart contracts and utilities used on Yearn architecture',
+        to: './vaults/smart-contracts/vault',
+      },
+      {
         title: 'Yearn SDK',
         description: 'Learn about the Yearn stack and how to use the SDK as an aggregator of many data sources.',
         to: './vaults/yearn-sdk/yearn-stack',
       },
+    ],
+  },
+  {
+    title: 'Discover',
+    rows: [
       {
-        title: 'Smart Contracts',
-        description: 'Find references to our smart contracts and several utilities part of the Yearn architecture',
-        to: './vaults/smart-contracts/vault',
+        title: 'Vaults & Strategies',
+        description: 'An overview on how Yearn Vaults and its strategies work',
+        to: './getting-started/products/yvaults/overview',
+      },
+      {
+        title: 'veYFI',
+        description: 'Lock YFI, stake on gauges, claim rewards: Learn how veYFI works.',
+        to: './contributing/governance/veyfi',
+      },
+      {
+        title: 'Partnership Program',
+        description: `Discover incentives for builders to integrate Yearn Vaults in their services`,
+        to: './partners/introduction',
       },
     ],
   }
