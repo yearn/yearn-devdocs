8251c5c77f88278b7a354c6ef317ec1dcec8fe56
Marco Guaspari Worms <marcogworms@gmail.com>
2023-07-29 00:34:04 +0200

    add yearn vaults v3 docs (#291)
    * add v3 docs
    
    * adjust sidebar v3 title
    
    * add quote style to repeated warning
    
    * improve quoting style
    
    * fix leftover header
    
    * add read more links to v3 docs
    
    * fix: wording
    
    * fix: grammar and small fixes
    
    * fix: space
    
    * fix: more fixes
    
    * fix: various
    
    * fixes
    
    * fix: smart modules
    
    Co-authored-by: Marco Guaspari Worms <marcogworms@gmail.com>
    
    * fix: punctuation
    
    * fix: debt manager & update title
    
    * fix: various fixes, eoa, multisig
    
    * fix: remove colon outside of special highlighting
    
    * fix: various
    
    * fixes
    
    * fix: standardize heading capitialization
    
    * fix internal link
    
    * fix casing
    
    * fix casing
    
    * improve wording
    
    * improve wording
    
    * fix: caps
    
    * fix: wording
    
    * feat: add vault factory to sidebar
    
    * fix: reorder sidebar
    
    * feat: update v3 docs
    
    * chore: update the spechs
    
    * Apply suggestions from code review
    
    * grammar fixes
    
    * Update v3.md
    
    * Update overview.md
    
    * test formula
    
    * fix math formula
    
    * apply standard style
    
    * typos
    
    * fix: wording
    
    * fix: update wording and rephrase info
    
    * fix: roles
    
    * updates
    
    * chore: update title caps
    
    * updates and fixes
    
    * updates and fixes
    
    * updates
    
    * updates
    
    * mistake
    
    * add: links
    
    * fix: typo
    
    * fix: duplicate info
    
    * fix: duplicate info
    
    * fix: spacing
    
    ---------
    
    Co-authored-by: Dark Ghosty <38810205+DarkGhost7@users.noreply.github.com>
    Co-authored-by: Schlagonia <schlagonia@gmail.com>
    Co-authored-by: 0xBasically <0xBasic@yearn.finance>

--- sidebars/sidebarsGettingStarted.js
+++ sidebars/sidebarsGettingStarted.js
@@ -14,6 +14,7 @@ module.exports = {
             'products/yvaults/vaults-and-strategies',
             'products/yvaults/vault-tokens',
             'products/yvaults/vault-factory',
+            'products/yvaults/v3',
           ],
         },
         {
