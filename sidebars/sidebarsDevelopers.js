6b3a6f5d917834beae568500955fe42e9c05d82c
Marco Guaspari Worms <marcogworms@gmail.com>
2023-09-05 00:40:00 -0300

    fix v3 sidebar (#347)
    * fix v3 sidebar
    
    * update overview with strategy writing guide
    
    * remove one strategy writing guide bc was duplicate
    
    * revert
    
    * fix identation

--- sidebars/sidebarsDevelopers.js
+++ sidebars/sidebarsDevelopers.js
@@ -32,6 +32,15 @@ module.exports = {
     'v2/hacking-with-yearn',
     'v2/fork-yearn-ui',
     'v2/yswaps',
-    'v3/strategy_writing_guide',
+    {
+      type: 'category',
+      label: 'V3',
+      items: [
+        'v3/overview',
+        'v3/strategy_development',
+        'v3/protocol_fees',
+        'v3/vault_management',
+      ],
+    },
   ],
 }
