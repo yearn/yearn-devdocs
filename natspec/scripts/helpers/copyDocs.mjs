122d6e62da2eb8c2929849d9645f07866604e1a1
Ross <therossgalloway@gmail.com>
2024-11-08 13:37:12 -0500

    new regex to clean output from script run.

--- natspec/scripts/helpers/copyDocs.mjs
+++ natspec/scripts/helpers/copyDocs.mjs
@@ -1,3 +1,4 @@
+import { execSync } from 'child_process'
 import path from 'path'
 import fs from 'fs'
 
@@ -110,8 +111,15 @@ export function copyDocs(outputBaseDir, tempFolder, contractsToUpdate) {
   traverseAndCopy(tempVydocDir, outputBaseDir, '.vy', contractPathMap)
 }
 
-// Helper function to build a map of contract names to their paths
 // this could fail if there are duplicate file names in nested folders.
+/**
+ * Builds a map of contract file paths.
+ *
+ * @param {Object} obj - The object representing the directory structure.
+ * @param {string} currentPath - The current path in the directory structure.
+ * @param {Array<string>} [contractsToUpdate] - An optional array of contract names to update.
+ * @returns {Object} A map where the keys are contract names and the values are their respective file paths.
+ */
 function buildContractPathMap(obj, currentPath, contractsToUpdate) {
   let contractPathMap = {}
   let baseContractNames = []
@@ -183,6 +191,17 @@ function traverseAndCopy(baseDir, outputBaseDir, extension, contractMap) {
   }
 }
 
+/**
+ * Processes a markdown file by removing forge prefixes from its basename,
+ * constructing the contract name, and copying and modifying the file if the
+ * contract name exists in the contract map.
+ *
+ * @param {string} entry - The name of the markdown file.
+ * @param {string} entryPath - The path to the markdown file.
+ * @param {string} extension - The extension to be appended to the file basename.
+ * @param {Object} contractMap - A map of contract names to their respective data.
+ * @param {string} outputBaseDir - The base directory where the modified file will be copied.
+ */
 function processMarkdownFile(
   entry,
   entryPath,
@@ -206,6 +225,31 @@ function processMarkdownFile(
   }
 }
 
+/**
+ * Removes prefixes added by Forge from the given file base name.
+ *
+ * @param {string} fileBaseName - The base name of the file.
+ * @returns {string} - The file base name without Forge prefixes.
+ */
+function removeForgePrefixes(fileBaseName) {
+  const prefixes = ['contract.', 'abstract.', 'interface.']
+  for (const prefix of prefixes) {
+    if (fileBaseName.startsWith(prefix)) {
+      return fileBaseName.slice(prefix.length)
+    }
+  }
+  return fileBaseName
+}
+
+/**
+ * Copies a file to a new location, modifies its content, and saves it.
+ *
+ * @param {string} outputBaseDir - The base directory where the file will be copied to.
+ * @param {string} entryPath - The path of the file to be copied.
+ * @param {string} contractName - The name of the contract associated with the file.
+ * @param {string} extension - The file extension of the original file.
+ * @param {Object} contractMap - A mapping of contract names to their relative paths.
+ */
 function copyAndModifyFile(
   outputBaseDir,
   entryPath,
@@ -225,27 +269,98 @@ function copyAndModifyFile(
 
   // Copy the documentation file
   fs.copyFileSync(entryPath, destFilePath)
-  console.log(`Copied ${contractName}`)
+  console.log(`\nCopying ${contractName}`)
 
   // Add the specified line to the top of the copied file
-  const disableLintLine = '<!-- markdownlint-disable MD024 MD034 MD036 -->\n'
-  const fileContent = fs.readFileSync(destFilePath, 'utf8')
-  fs.writeFileSync(destFilePath, disableLintLine + fileContent)
-  // console.log(`Added markdownlint disable line to ${destFilePath}`)
+  let fileContent = fs.readFileSync(destFilePath, 'utf8')
+
+  // Add the specified line to the top of the copied file
+  fileContent = addDisableLintLine(fileContent)
+
+  // Process the inherits line
+  fileContent = processInheritsLine(fileContent)
+
+  // Replace curly brackets with backticks
+  fileContent = replaceCurlyBrackets(fileContent)
+
+  // Write the modified content back to the file
+  fs.writeFileSync(destFilePath, fileContent)
 }
 
 /**
- * Removes prefixes added by Forge from the given file base name.
+ * Adds a markdownlint disable line to the beginning of the provided file content.
  *
- * @param {string} fileBaseName - The base name of the file.
- * @returns {string} - The file base name without Forge prefixes.
+ * @param {string} fileContent - The content of the file to which the disable lint line will be added.
+ * @returns {string} The file content with the markdownlint disable line prepended.
  */
-function removeForgePrefixes(fileBaseName) {
-  const prefixes = ['contract.', 'abstract.', 'interface.']
-  for (const prefix of prefixes) {
-    if (fileBaseName.startsWith(prefix)) {
-      return fileBaseName.slice(prefix.length)
+function addDisableLintLine(fileContent) {
+  const disableLintLine = '<!-- markdownlint-disable MD024 MD034 MD036 -->\n'
+  return disableLintLine + fileContent
+}
+
+/**
+ * Processes the first 10 lines of the given file content to find and update the '**Inherits:**' line.
+ * If the '**Inherits:**' line is found, it replaces any markdown links with just the text and updates the line.
+ * If the '**Inherits:**' line has no content on the same line, it checks the next line for content.
+ *
+ * @param {string} fileContent - The content of the file to process.
+ * @returns {string} - The updated file content with the processed '**Inherits:**' line.
+ */
+function processInheritsLine(fileContent) {
+  const lines = fileContent.split('\n')
+  for (let i = 0; i < Math.min(10, lines.length); i++) {
+    let line = lines[i].trim()
+
+    // Match '**Inherits:**' possibly followed by content
+    const inheritsMatch = line.match(/^\*\*Inherits:\*\*(.*)/)
+
+    if (inheritsMatch) {
+      let inheritsText = inheritsMatch[1].trim()
+      let contentLineIndex = i
+
+      // If there's no content on the same line, check the next line
+      if (!inheritsText && i + 1 < lines.length) {
+        contentLineIndex = i + 1
+        inheritsText = lines[contentLineIndex].trim()
+      }
+
+      // Replace links with just the text
+      inheritsText = inheritsText.replace(/\[([^\]]+)\]\([^)]+\)/g, '$1')
+
+      console.log(
+        `Replacing inherits line: ${lines[contentLineIndex]} with **Inherits:** ${inheritsText}`
+      )
+
+      // Update the lines
+      lines[i] = `**Inherits:** ${inheritsText}`
+
+      // If content was on the next line, remove that line
+      if (contentLineIndex !== i) {
+        lines.splice(contentLineIndex, 1)
+      }
+
+      break
     }
   }
-  return fileBaseName
+  return lines.join('\n')
+}
+
+/**
+ * Replaces curly brackets in the given file content with backticks.
+ *
+ * This function searches for all occurrences of text enclosed in curly brackets
+ * (e.g., {example}) and replaces them with the same text enclosed in backticks
+ * (e.g., `example`). It logs each replacement to the console.
+ *
+ * @param {string} fileContent - The content of the file in which to replace curly brackets.
+ * @returns {string} The modified file content with curly brackets replaced by backticks.
+ */
+function replaceCurlyBrackets(fileContent) {
+  const curlyBracketsRegex = /\{([^\r\n{}]{0,20})\}/g
+  let match
+  while ((match = curlyBracketsRegex.exec(fileContent)) !== null) {
+    console.log(`Replacing: ${match[0]} with \`${match[1]}\``)
+    fileContent = fileContent.replace(match[0], `\`${match[1]}\``)
+  }
+  return fileContent
 }
