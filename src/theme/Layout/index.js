d840dd9a33d40819a2459cb1e74767b71b8f4d77
Ross <therossgalloway@gmail.com>
2025-01-21 11:13:29 -0500

    fix: picked wrong merge conflict choice

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -6,26 +6,5 @@ import Layout from '@theme-original/Layout'
  * Edit this component if you want to change the main layout of your site
  */
 export default function LayoutWrapper(props) {
-  const { siteConfig } = useDocusaurusContext()
-  const { branchName, isDev } = siteConfig.customFields
-
-  return (
-    <>
-      {isDev && (
-        <div
-          style={{
-            position: 'fixed',
-            top: 0,
-            right: 0,
-            background: 'yellow',
-            padding: '5px',
-            color: 'black',
-          }}
-        >
-          {branchName}
-        </div>
-      )}
-      <Layout {...props} />
-    </>
-  )
+  return <Layout {...props} />
 }
